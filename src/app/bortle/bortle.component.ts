44d98a5162f8b4bc34fcc25d12004f241f4b2aaa
Peter Jorgensen <prjorgen@gmail.com>
2025-04-15 19:18:46 -0600

    but not always capitalized

--- src/app/bortle/bortle.component.ts
+++ src/app/bortle/bortle.component.ts
@@ -13,13 +13,13 @@ export interface Bortle {
 }
 
 @Component({
-  selector: 'app-Bortle',
+  selector: 'app-bortle',
   imports: [MatSliderModule, MatRadioModule, FormsModule, MatTooltipModule, CommonModule],
-  templateUrl: './Bortle.component.html',
-  styleUrl: './Bortle.component.scss'
+  templateUrl: './bortle.component.html',
+  styleUrl: './bortle.component.scss'
 })
 export class BortleComponent {
-  Bortles: Record<number, Bortle> = {
+  bortles: Record<number, Bortle> = {
     0: { class: 0, name: 'Space', description: 'No atmospheric interference.', example: 'While not an official Bortle rating, this value indicates a view of the stars from space, without the interference of an atmosphere.' },
     1: { class: 1, name: 'Dark Sky', description: 'Excellent dark-sky site.', example: 'Many remote sites in Utah, especially remote mountain valleys in the Northwest corner of the state, earn a Bortle rating of 1 and present truly stunning views of the stars.' },
     2: { class: 2, name: 'Dark Site', description: 'Typical truly dark site.', example: 'Bryce Canyon National Park, a favorite of Utah astrophotographers, is largely classified with a Bortle rating of 2.' },
@@ -29,23 +29,23 @@ export class BortleComponent {
     6: { class: 6, name: 'Bright Suburban', description: 'Bright suburban sky with constant glow near the horizon.', example: 'The city of St. George, Utah has a Bortle rating of 6 near I-15.' },
     7: { class: 7, name: 'Suburban Transition', description: 'Suburban sky near a big city, strong light in all directions.', example: 'Where the Salt Lake Valley is at its brightest, it has a Bortle rating of 7.' },
     8: { class: 8, name: 'City', description: 'Sky over a large city with constant glow all around.', example: 'The open terrain around  Phoenix, Arizona with the bright city lights earn a Bortle rating of 8.' },
-    9: { class: 9, name: 'Inner City', description: 'Bright skies over a large inner city.', example: 'The neon lights and busy motion of Las Vegas, Nevada earn a rating of 9.'}
+    9: { class: 9, name: 'Inner City', description: 'Bright skies over a large inner city.', example: 'The neon lights and busy motion of Las Vegas, Nevada earn a Bortle rating of 9.'}
   }
 
-  BortlesArray = Object.values(this.Bortles);
+  bortlesArray = Object.values(this.bortles);
 
   selectedValue: number = 0;
-  Bortle = this.Bortles[this.selectedValue]
+  bortle = this.bortles[this.selectedValue]
   
   constructor() {
     this.selectedValue = 0;
   }
 
-  @Output() BortleChanged: EventEmitter<number> = new EventEmitter<number>();
+  @Output() bortleChanged: EventEmitter<number> = new EventEmitter<number>();
 
   selectedValueChanged() {
-    this.BortleChanged.emit(this.selectedValue);
-    this.Bortle = this.Bortles[this.selectedValue]
+    this.bortleChanged.emit(this.selectedValue);
+    this.bortle = this.bortles[this.selectedValue]
   }
 
   formatLabel: string = `Bortle class ${this.selectedValue}`;  
